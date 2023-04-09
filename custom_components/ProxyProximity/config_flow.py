db431dacad52aec2397094152b1fd3f9c93209f2
danps <36139437+danps1@users.noreply.github.com>
2023-04-10 00:55:58 +0700

    Update config_flow.py

--- custom_components/ProxyProximity/config_flow.py
+++ custom_components/ProxyProximity/config_flow.py
@@ -1,53 +1,110 @@
-import logging
-
-from homeassistant import config_entries
-from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.helpers import device_registry
+import homeassistant.helpers.config_validation as cv
 
-from .const import DOMAIN
+from .const import DOMAIN, KNOWN_DEVICES, DEVICE_TRACKERS, CONF_DEVICES, CONF_PROXIES, DEFAULT_NAME
 
-_LOGGER = logging.getLogger(__name__)
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_DEVICES): vol.All(cv.ensure_list, [cv.string]),
+        vol.Required(CONF_PROXIES): vol.All(cv.ensure_list, [cv.string]),
+    }
+)
 
-class BleDeviceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for BLE devices."""
+async def validate_devices(hass, devices):
+    known_devices = {}
+    device_registry = await hass.helpers.device_registry.async_get_registry()
+    for device in devices:
+        known_device = KNOWN_DEVICES.get(device)
+        if known_device:
+            known_devices[device] = known_device
+            continue
+        if len(device) != 17:
+            return None
+        if device in KNOWN_DEVICES.values():
+            return None
+        if device_registry.async_get_device({("mac", device)}, set()) is None:
+            return None
+        known_devices[device] = None
+    return known_devices
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for ProxyProximity."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     async def async_step_user(self, user_input=None):
-        """Handle a flow initialized by the user."""
-
-        if user_input is None:
-            # Show the initial form
-            return self._show_form()
-
-        # Validate the user input
-        errors = self._validate_user_input(user_input)
-        if errors:
-            return self._show_form(errors=errors)
-
-        # Save the user input as a new config entry
-        return self.async_create_entry(
-            title=user_input['name'],
-            data=user_input
-        )
-
-    def _show_form(self, errors=None):
-        """Show the config form to the user."""
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required("name"): str,
-                    vol.Required("identity_resolving_key"): cv.string,
-                }
-            ),
-            errors=errors or {},
-        )
-
-    def _validate_user_input(self, user_input):
-        """Validate the user input."""
+        """Handle the initial step."""
         errors = {}
 
-        # TODO: Validate user input here
-        if not user_input["identity_resolving_key"]:
-            errors["base"] = "identity_resolving_key is required"
+        if user_input is not None:
+            devices = user_input[CONF_DEVICES]
+            proxies = user_input[CONF_PROXIES]
 
-        return errors
+            known_devices = await validate_devices(self.hass, devices)
+            if known_devices is None:
+                errors["base"] = "invalid_devices"
+            else:
+                self.context["data"] = {
+                    CONF_DEVICES: known_devices,
+                    CONF_PROXIES: proxies,
+                }
+                return self.async_create_entry(title=DEFAULT_NAME, data=self.context["data"])
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=DATA_SCHEMA,
+            errors=errors,
+        )
+
+    async def async_step_import(self, import_config):
+        """Handle import from config file."""
+        if not import_config:
+            return await self.async_step_user()
+
+        devices = import_config.get(CONF_DEVICES, [])
+        proxies = import_config.get(CONF_PROXIES, [])
+
+        known_devices = await validate_devices(self.hass, devices)
+        if known_devices is None:
+            return self.async_abort(reason="invalid_devices")
+
+        return self.async_create_entry(
+            title=DEFAULT_NAME,
+            data={
+                CONF_DEVICES: known_devices,
+                CONF_PROXIES: proxies,
+            },
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle option flow for ProxyProximity."""
+
+    def __init__(self, config_entry):
+        """Initialize."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        known_devices = self.config_entry.data.get(CONF_DEVICES, {})
+        device_trackers = self.hass.data.get(DEVICE_TRACKERS, {})
+        devices = []
+        for mac_address, device in known_devices.items():
+            if mac_address in device_trackers:
+                devices.append(
+                    {
+                        "mac": mac_address,
+                        "name": device.get("name") if device else "",
+                        "
